af9dd75a8bab97fea6777a64c6cb70dbfff0c04a
MOHAMED LAMINE BELAROUCI <154247237+maxie-dz@users.noreply.github.com>
2025-05-30 20:45:34 +0100

    Update reset_request.php

--- reset_request.php
+++ reset_request.php
@@ -46,8 +46,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $mail->isSMTP();
                 $mail->Host = 'smtp.gmail.com';
                 $mail->SMTPAuth = true;
-                $mail->Username = 'lamminne2004@gmail.com';
-                $mail->Password = 'vwgk sofv icjg ldar'; // your app password
+                $mail->Username = '';
+                $mail->Password = ''; // your app password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                 $mail->Port = 587;
 
