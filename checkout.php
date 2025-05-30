35a2f85dcb916c102ed41f141abfd5fa6b3036d7
MOHAMED LAMINE BELAROUCI <154247237+maxie-dz@users.noreply.github.com>
2025-05-30 20:44:54 +0100

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -66,12 +66,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $mail->isSMTP();
                 $mail->Host = 'smtp.gmail.com'; // Replace with your SMTP
                 $mail->SMTPAuth = true;
-                $mail->Username = 'lamminne2004@gmail.com';
-                $mail->Password = 'vwgk sofv icjg ldar';
+                $mail->Username = '';
+                $mail->Password = '';
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                 $mail->Port = 587;
 
-                $mail->setFrom('lamminne2004@gmail.com', 'WordCrate');
+                $mail->setFrom('', 'WordCrate');
                 $mail->addAddress($email);
 
                 $downloadUrl = "http://localhost:8888/download.php?id=$ebook_id&email=" . urlencode($email);
